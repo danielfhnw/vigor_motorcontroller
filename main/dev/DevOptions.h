da695376769d201475dde26b69e7ac9f5cabb4dc
danielfhnw <daniel.jakober@fhnw.ch>
2026-05-14 15:39:50 +0200

    added wd disable option

--- main/dev/DevOptions.h
+++ main/dev/DevOptions.h
@@ -4,3 +4,7 @@
 #define INVERTED_L true
 #define INVERTED_R true
 #define PLOTTER false
+#define DISABLE_WD_L_F false
+#define DISABLE_WD_R_F false
+#define DISABLE_WD_L_B false
+#define DISABLE_WD_R_B false
--- main/src/Watchdog.h
+++ main/src/Watchdog.h
@@ -10,12 +10,16 @@ class Watchdog {
         Watchdog& operator=(const Watchdog& other);
         bool check(uint16_t pos);
         bool get_fired();
+        bool get_disabled();
+        void disable() {_disabled = true;}
+        void enable() {_disabled = false;}
         void reset();
         void reset_count();
     private:
         uint16_t _old_pos;
         uint16_t _count;
         bool _fired;
+        bool _disabled = false;
         const int _cnt_tresh = 10;
         const int _pos_tresh = 25;
 };
