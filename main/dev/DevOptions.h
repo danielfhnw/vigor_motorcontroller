d51219d6d3c795e0e92583d3b9047eed44268dd7
danielfhnw <daniel.jakober@fhnw.ch>
2026-08-28 13:57:36 +0200

    removed dipswitch functions from devoptions

--- main/dev/DevOptions.h
+++ main/dev/DevOptions.h
@@ -1,10 +1,4 @@
 #pragma once 
 
 // define all constants
-#define INVERTED_L true
-#define INVERTED_R true
 #define PLOTTER false
-#define DISABLE_WD_L_F false
-#define DISABLE_WD_R_F false
-#define DISABLE_WD_L_B false
-#define DISABLE_WD_R_B false
