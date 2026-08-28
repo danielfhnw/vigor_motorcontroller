e42b082150aefc6a04b1a4a5b92dbee6a5294e0d
danielfhnw <daniel.jakober@fhnw.ch>
2026-08-28 14:11:11 +0200

    added einschwingzeit für mux

--- main/src/PinMap.cpp
+++ main/src/PinMap.cpp
@@ -22,5 +22,6 @@ bool Pinmap::read_dip_switch(uint8_t channel) {
   digitalWrite(MUX_A0, bitRead(channel, 0));
   digitalWrite(MUX_A1, bitRead(channel, 1));
   digitalWrite(MUX_A2, bitRead(channel, 2));
+  delayMicroseconds(5);
   return digitalRead(MUX_SEL);
 }
